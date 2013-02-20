a23df87ef0a78afc31290d15f4c345e7f999880f
Jesús Contreras <chudacontreras@gmail.com>
2013-02-19 22:39:29 -0430

    Segundo commit agregado los css y cradas las clases para la pagina principal

---
+++ application/views/index.php
@@ -0,0 +1,38 @@
+<div id="contenido_principal">
+<style type="text/css">
+<!--
+.Estilo5 {font-size: 12px}
+.Estilo6 {font-size: 12px; color: #999999; }
+.Estilo7 {color: #999999}
+.Estilo8 {color: #666666}
+.Estilo9 {font-size: 15px; color: #666666; }
+.Estilo10 {font-size: 24px; color: #0099FF; }
+-->
+</style>
+<div id="columnaDos">
+      <h2 class="Estilo10">Bienvenidos a Ministerio Cristiano Ciudad Fuerte </h2>
+	  <p class="indent Estilo7 Estilo10">&nbsp;</p>
+	  <div class="Estilo6">
+              <p><img src="<?php echo base_url();?>images/paste3.jpg"></p>
+	    <p class="Estilo9 Estilo8">La Visión G12 (Gobierno de los Doce) se fundamenta en la Gran Comisión dada por Jesús. Su principal objetivo es hacer de cada creyente un líder que a su vez transmita el carácter de Cristo y conforme un equipo de doce personas. La Visión está estructurada en cuatro principios bíblicos: ganar, consolidar, discipular y enviar, lo que llamamos "la escalera del éxito". Estos cuatro sencillos principios buscan ganar al pecador para que se arrepienta y reciba la salvación por gracia, consolidarlo (afirmarlo) para que permanezca en su decisión por Cristo y discipularlo para que se prepare en la tarea de extender el mensaje del evangelio y así, enviarlo a predicar las buenas nuevas de salvación. El G12 nació a través de una visión que el Señor le dio al pastor colombiano César Castellanos Domínguez, y durante estos últimos anos el crecimiento de la Iglesia cristiana en esa nación, en Venezuela y en el mundo entero ha ido en avanzada indetenible. Un sin número de ministerios han sido testigos del crecimiento y multiplicación de miembros que ha desarrollado la Visión del Gobierno de los Doce, pues esta herramienta resulta eficaz para ganar y sobre todo para retener el fruto: miles de nuevos creyentes. </p>
+	    <hr size="2">
+	  </div>
+	  <div>
+        <h3 class="Estilo10">Nuestra Visíon </h3>
+	    <p align="right" class="Estilo9 Estilo5 Estilo7"><strong>“El verdadero liderazgo requiere un trabajo intenso y profundo,en el interior de cada persona”</strong></p>
+	    <p align="right" class="Estilo9"><strong>Pastor César Castellanos</strong> </p>
+	    <p class="Estilo9 Estilo5 Estilo7">El modelo de los doce siempre ha estado en el corazón de Dios, y su número simboliza gobierno. Cuando existe ese gobierno hay orden, y se genera un escenario donde todos saben cual es su función, reconocen sus capacidades y son útiles en la tarea que se les delega.</p>
+	    <p class="Estilo9 Estilo5 Estilo7">Esta es la misión del Gobierno de los Doce:</p>
+	    <p class="Estilo9 Estilo5 Estilo7">Administrar. El rey Salomón estableció doce gobernadores para su reino. Cada uno de ellos estaba obligado a abastecerle a él y a su casa de todo lo necesario para vivir durante el año. (1 Reyes 4:7)</p>
+	    <p class="Estilo9 Estilo5 Estilo7">Restaurar. “Y tomando Elías doce piedras, conforme al número de las tribus de los hijos de Jacob… edificó con las piedras un altar a Jehová” (1 Reyes 18:31ª.32ª). Y después de preparar el holocausto, procedió a ofrecerlo a Dios en sacrificio. El Señor no tardó en responderle al profeta, y fue a través de este acto que vino la reconciliación del pueblo con Dios.</p>
+	    <p class="Estilo9 Estilo5 Estilo7">Reconciliar. “He aquí yo os envió el profeta Elías, antes que venga el día de Jehová, grande y terrible. Él hará volver el corazón de los padres hacia los hijos, y el corazón de los hijos hacia los padres, no sea que yo venga y hiera la tierra con maldición” (Malaquías 4:5.6)</p>
+	    <p class="Estilo9 Estilo5 Estilo7">El líder es la clave. Esta es una de las grandes enseñanzas que nos da Jesús, pues el siempre tuvo en cuenta a  las personas no por lo que ellas aparentaban ser, sino por el potencial que Él sabía que existía en cada una de ellas; un potencial que provenía del mismo soplo divino, pero estaba opacado por las diferentes circunstancias que los habían embargado.</p>
+	    <p class="Estilo9 Estilo5 Estilo7">Dios quiere confiarnos hermosas vidas, las cuales gracias a su fe, amor y empeño serán transformadas en valiosos líderes que levantarán una nueva generación para Dios</p>
+	    <hr size="2">
+	    
+	    <div align="center"><span class="Estilo5"><a href="1">Pagina Siguiente</a> - <a href="2">Pagina Anterior	</a></span></div>
+  </div>
+	  <center><p>&nbsp;</p>
+      </center>
+</div>
+</div>
